7ce2503345fdac0e42af9315b63abbd00c8bd12a
nani3013 <undrunarendra3013@gmai.com>
2024-12-05 13:11:01 +0530

    sg

---
+++ 50-app-alb/variables.tf
@@ -0,0 +1,28 @@
+
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+
+variable "app_alb_tags" {
+    default = {
+        component = "app-alb"
+    }
+}
+
+
+variable "zone_name" {
+    default = "nani30.online"
+}
